93d3b725145bd25329fd472ca41b3935d87197c6
Dmitry <proctvvv@gmail.com>
2023-07-13 10:53:42 +0300

    My track

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/WARTEstBuild.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/WebAR_tets_host.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
